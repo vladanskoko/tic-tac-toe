0116784fcc4273d1466f53dee701b7a54b2de76d
Vladan Skoko <vladan.skoko@axiomq.com>
2025-02-11 14:04:55 +0100

    Add two-way binding for player names

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
-          <Player name="Player 1" symbol="X"></Player>
-          <Player name="Player 2" symbol="Y"></Player>
+          <Player initialName="Player 1" symbol="X"></Player>
+          <Player initialName="Player 2" symbol="Y"></Player>
         </ol>
       </div>
     </main>
